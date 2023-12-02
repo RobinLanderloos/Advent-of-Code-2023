2317d3436354825ba009818d079abfa5002cf616
Robin Landerloos <rlanderloos@gmail.be>
2023-12-02 20:36:15 +0100

    Split up code and added documentation

---
+++ day-1-trebuchet/src/puzzle_input_handler.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub struct PuzzleInputHandler {
+    path: String,
+}
+
+impl PuzzleInputHandler {
+    /// Returns a new PuzzleInputHandler instance for handling the given path
+    pub fn new(path: String) -> PuzzleInputHandler {
+        return PuzzleInputHandler { path: path };
+    }
+
+    /// Returns the calibration from the puzzle input
+    pub fn get_calibration_from_puzzle_input(&self) -> i32 {
+        let mut calibration: i32 = 0;
+        match PuzzleInputHandler::read_lines(self.path.to_string()) {
+            Ok(lines) => {
+                for line in lines {
+                    match line {
+                        Ok(l) => {
+                            let line_calibration =
+                                PuzzleInputHandler::get_calibration_from_line(&l);
+                            calibration += line_calibration;
+                            println!("Line Calibration: {}", line_calibration);
+                        }
+                        Err(error) => {
+                            println!("Error reading line: {}", error);
+                        }
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Error reading file: {}", error);
+            }
+        }
+
+        return calibration;
+    }
+
+    /// Returns the calibration from the given line
+    fn get_calibration_from_line(line: &str) -> i32 {
+        let radix: u32 = 10; // Base 10
+        let mut first: char = '0';
+        let mut last: char = '0';
+
+        // loop over every character of string
+        for character in line.chars() {
+            if character.is_digit(radix) {
+                let digit: char = character;
+
+                // Only set this when the first digit is found
+                if first == '0' {
+                    first = digit;
+                }
+
+                last = digit; // Set on every digit found
+            }
+        }
+
+        let combined = format!("{}{}", first, last);
+        println!("First: {}, Last: {}, Combined: {}", first, last, combined);
+
+        // No need for error handling as we know the string is a number
+        return combined.parse::<i32>().unwrap();
+    }
+
+    // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+    /// Returns the lines from the given file
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+}
